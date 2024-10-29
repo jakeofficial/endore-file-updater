e25a791e71975d3fbc28e610866feb168fca871a
jakub <>
2024-10-29 23:13:25 +0100

    Update UpdaterFilesCheckController.php

--- app/Http/Controllers/UpdaterFilesCheckController.php
+++ app/Http/Controllers/UpdaterFilesCheckController.php
@@ -11,11 +11,13 @@ class UpdaterFilesCheckController extends Controller
     public function __invoke(Request $request)
     {
 
-        Log::debug('Raw input: ' . $request->getContent());
-        Log::debug('Headers: ', $request->headers->all());
-        $files_to_check = $request->post('files_to_check');
+        $jsonData = json_decode($request->getContent(), true);
+        $files_to_check = $jsonData['files_to_check'] ?? null;
+//        Log::debug('Raw input: ' . $request->getContent());
+//        Log::debug('Headers: ', $request->headers->all());
+//        $files_to_check = $request->post('files_to_check');
 
-        Log::debug('Files to check: ', $request->post());
+        Log::debug('Files to check: ', $files_to_check);
 
         $latestFiles = UpdaterFile::pluck('hash', 'name')->toArray();
 
